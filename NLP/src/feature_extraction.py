199de2e35922320b78a71a6c2bd9537a1ca73e7c
Ануар (Аник) <102426785+GreenAlen22@users.noreply.github.com>
2024-02-08 20:27:53 +0600

    Add files via upload

--- NLP/src/feature_extraction.py
+++ NLP/src/feature_extraction.py
@@ -1,14 +1,21 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 class FeatureExtractor:
+#   создание атрибута в качестве экземпляра TfidfVectorizer
     vectorizer = TfidfVectorizer()
 
+#   метод принимает на вход список текстов (texts) и использует vectorizer для 
+#   вычисления признаков этих текстов. Метод fit_transform сначала обучается 
+#   на переданных текстах (вычисляет IDF для каждого слова), а затем преобразует 
+#   тексты в соответствующие векторы TF-IDF
     @staticmethod
     def extract_features(texts):
         features = FeatureExtractor.vectorizer.fit_transform(texts)
         return features
 
-    @staticmethod
+#   Этот метод такой же как extract_features, но нужен для преобразования новых 
+#   текстовых данных, используя уже обученный vectorizer. Метод transform не обучается
+#   на новых данных, он просто меняет их в ранее вычисленными IDF значениями
     def transform_new_text(new_texts):
         new_features = FeatureExtractor.vectorizer.transform(new_texts)
         return new_features
--- NLP/src/main.py
+++ NLP/src/main.py
@@ -1,24 +1,36 @@
 import pandas as pd
+#   предназначен для предварительной обработки текстовых данных
 from preprocess import TextPreprocessor
+#   используется для извлечения признаков из предобработанных текстов
 from feature_extraction import FeatureExtractor
+#   представляет собой классификатор текста.
 from model import TextClassifier
+#   используется для разделения данных на обучающую и тестовую выборки.
 from sklearn.model_selection import train_test_split
 
+
+#   Читает csv файл и возвращает Метки признака
 def load_data(filename):
     data = pd.read_csv(filename)
     return data['Review'], data[' Sentiment']
 
 def main():
-    # Load and preprocess data
+#   Загрузка и предварительная обработка данных
     reviews, sentiments = load_data('data/reviews.csv')
     preprocessed_reviews = [TextPreprocessor.preprocess(review) for review in reviews]
     
+#   Извлекает признаки
     features = FeatureExtractor.extract_features(preprocessed_reviews)
     
+#   Разделяет данные на обучающую и тестовую выборки
+#   test_size=0.2 означает, что 20% данных для тестирования, а оставшиеся для обучения 
     X_train, X_test, y_train, y_test = train_test_split(features, sentiments, test_size=0.2, random_state=42)
     
+#   создаем экземпляр
     classifier = TextClassifier()
+#   обучает его выборке
     classifier.train(X_train, y_train)
+#   оценивает проиизводительность 
     classifier.evaluate(X_test, y_test)
 
 if __name__ == "__main__":
